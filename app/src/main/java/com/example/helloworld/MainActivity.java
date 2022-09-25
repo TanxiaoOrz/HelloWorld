e512cb45c5b76cd2517b406e9de0ce15701e29fd
Tanxiao <13671985248@163.com>
2022-09-25 17:11:41 +0800

    maybe use

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.helloworld;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText allLoan,yearInterestRate,loanYear;
+    private Button calLoan;
+    private ListView ShowDebx,ShowDebj;
+    private TextView debxTotalInterest;
+    private TextView debjTotalInterest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv3=findViewById(R.id.tvhello);
-        tv3.setSelected(true);
     }
 }
